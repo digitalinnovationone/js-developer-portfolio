682f1b1136d609e6a9ae58363a1f162b34b1f851
renanjpaula <renanjohannsen@gmail.com>
2022-08-16 08:12:22 -0300

    Dinamizando a softSkills

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,6 +1,7 @@
 
 async function fetchProfileData() {
     const url = 'https://raw.githubusercontent.com/digitalinnovationone/js-developer-portfolio/main/data/profile.json';
-    const fetching = await fetch(url)
-    return await fetching.json()
+    const response = await fetch(url)
+    const profileData = await response.json()
+    return profileData
 }
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,34 @@
 
+function updateProfileInfo(profileData) {
+    const photo = document.getElementById('profile.photo')
+    photo.src = profileData.photo
+    photo.alt = profileData.name
+
+    const name = document.getElementById('profile.name')
+    name.innerText = profileData.name
+
+    const job = document.getElementById('profile.job')
+    job.innerText = profileData.job
+
+    const location = document.getElementById('profile.location')
+    location.innerText = profileData.location
+
+    const phone = document.getElementById('profile.phone')
+    phone.innerText = profileData.phone
+    phone.href = `tel:${profileData.phone}`
+
+    const email = document.getElementById('profile.email')
+    email.innerText = profileData.email
+    email.href = `mailto:${profileData.email}`
+}
+
+function updateSoftSkills(profileData) {
+    const softSkills = document.getElementById('profile.skills.softSkills')
+    softSkills.innerHTML = profileData.skills.softSkills.map(skill => `<li>${skill}</li>`).join('')
+}
+
 (async () => {
     const profileData = await fetchProfileData()
-    console.log(profileData)
+    updateProfileInfo(profileData)
+    updateSoftSkills(profileData)
 })()
