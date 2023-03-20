74078faac6017116d18ddd0d7feff632f5c0495e
Jandui Junior <jandui@tecnologiageo.com.br>
2023-03-20 16:41:38 -0300

    conexão de dados com api e atualização de infos

--- assets/js/mains.js
+++ assets/js/mains.js
@@ -1,7 +1,32 @@
 
+function updateProfileInfos(profileData) {
+    const photo = document.getElementById('profile_photo')
+    photo.src = profileData.photo
+    photo.alt = `Foto de ${profileData.nome}`
+
+    const name = document.getElementById('profile_name')
+    //Também é possível utilizar textContent para substituir o texto no elemento, tal como o innerText; O primeiro, mantém a formatação original do elemento. Ex.: Se ele tiver quebras de linhas, manterá essa quebra.
+    name.innerText = profileData.nome
+
+    const job = document.getElementById('profile_job')
+    job.innerText = profileData.job
+    
+    const location = document.getElementById('profile_location')
+    location.innerText = profileData.location
+    
+    const phone = document.getElementById('profile_phone')
+    phone.innerText =profileData.phone
+    phone.href = `tel:${profileData.phone}`
+
+    const email = document.getElementById('profile_email')
+    email.innerText = profileData.email
+    email.href = `mailto:${profileData.phone}`
+}
+
 (async () => {
 
     const profileData = await fetchProfileData()
-    console.log(profileData.experiencias)
-
+    console.log(profileData)
+    updateProfileInfos(profileData)
 })()
+
