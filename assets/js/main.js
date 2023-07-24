eb3748be6d61929f69412ec823f5929a4780f5e1
Rafael Schenkel <rafaschenkel.dev@gmail.com>
2023-07-24 14:46:10 -0300

    Conclusao da implementação do preenchimento dos dados dinamicos

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,16 @@
 (async () => {
   const profileData = await fetchProfileData();
   updateInfos(profileData);
+  updateHardSkills(profileData);
+  updateSoftSkills(profileData);
+  updatePortfolio(profileData);
+  updateExperienceProfessional(profileData);
 })();
 
 function updateInfos(profileData) {
+  const photo = document.querySelector('#profile-photo');
+  photo.setAttribute('src', `${profileData.photo}`);
+
   const name = document.querySelector('#profile-name');
   name.innerText = profileData.name;
 
@@ -27,4 +34,64 @@ function updateInfos(profileData) {
   const email = document.querySelector('#profile-email');
   email.setAttribute('href', `mailto:${profileData.email}`);
   email.innerText = profileData.email;
+}
+
+function updateHardSkills(profileData) {
+  const ulHardSkills = document.querySelector('#profile-hardskills');
+  const hardSkills = profileData.skills.hardSkills
+    .map(hard => {
+      return `
+        <li>
+          <img class="tools-img html" src="${hard.logo}"
+            alt="icone ${hard.name}">
+        </li>
+      `;
+    })
+    .join('');
+  ulHardSkills.innerHTML = hardSkills;
+}
+
+function updateSoftSkills(profileData) {
+  const ulSoftSkills = document.querySelector('#profile-softskills');
+  const softSkills = profileData.skills.softSkills
+    .map(soft => {
+      return `
+        <li>${soft}</li>
+      `;
+    })
+    .join('');
+  ulSoftSkills.innerHTML = softSkills;
+}
+
+function updatePortfolio(profileData) {
+  const ulProjects = document.querySelector('#projects');
+  const projects = profileData.portfolio
+    .map(proj => {
+      return `
+        <li class="project">
+          <h3 class="project-title">${proj.name}</h3>
+          <a href="${proj.url}"
+              target="_blank">https://github.com/rafaschenkel/devsteam</a>
+        </li>
+      `;
+    })
+    .join('');
+  ulProjects.innerHTML = projects;
+}
+
+function updateExperienceProfessional(profileData) {
+  const experiencies = document.querySelector('#experiencies');
+  const expProfessional = profileData.professionalExperience
+    .map(exp => {
+      return `
+      <li class="experience">
+        <h3>${exp.name}</h3>
+        <p class="company">${exp.company}</p>
+        <p class="time">${exp.period}</p>
+        <p class="descriptio">${exp.description}</p>
+      </li>
+    `;
+    })
+    .join('');
+  experiencies.innerHTML = expProfessional;
 }
