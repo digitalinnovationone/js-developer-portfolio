26f2bc9578ff6da17f392de5977651fd6de04871
Rafael Schenkel <rafaschenkel.dev@gmail.com>
2023-07-24 13:16:22 -0300

    Configurando api.js

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,7 +1,8 @@
-
 async function fetchProfileData() {
-    const url = 'https://raw.githubusercontent.com/digitalinnovationone/js-developer-portfolio/main/data/profile.json';
-    const response = await fetch(url)
-    const profileData = await response.json()
-    return profileData
+  const url = 'https://raw.githubusercontent.com/rafaschenkel/js-developer-portfolio/schenkel/data/profile.json';
+  const dados = await (await fetch(url)).json();
+  console.log(dados);
+  return await dados;
 }
+
+fetchProfileData();
