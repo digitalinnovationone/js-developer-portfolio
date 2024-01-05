33a605bb100ed3c641968fdc6fc3528d8d3327fb
Pedrolustosa <pedroeternalss@gmail.com>
2024-01-05 20:58:24 -0300

    feat: Creation of portfolio according to my data and validation of knowledge

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,6 +1,6 @@
 
 async function fetchProfileData() {
-    const url = 'https://raw.githubusercontent.com/digitalinnovationone/js-developer-portfolio/main/data/profile.json';
+    const url = 'data/profile.json';
     const response = await fetch(url)
     const profileData = await response.json()
     return profileData
