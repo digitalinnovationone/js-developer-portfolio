b5b39a234b3dc099973115731d5a293821bcf0b4
Pedro Henrique Bianco Araújo <152442224+Pedro-Bianco@users.noreply.github.com>
2024-02-28 13:28:30 -0300

    Update api.js

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,6 +1,6 @@
 
 async function fetchProfileData() {
-    const url = 'https://raw.githubusercontent.com/digitalinnovationone/js-developer-portfolio/main/data/profile.json';
+    const url = 'https://raw.githubusercontent.com/digitalinnovationone/js-developer-portfolio/aa0fe75149e26b1dd6ff20c2ae78eafd38ab609f/data/profile.json';
     const response = await fetch(url)
     const profileData = await response.json()
     return profileData
